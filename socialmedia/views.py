9536f1731305fe68ca111a768bbe2031985ac8d8
Himanshu <himanshughodse@gmail.com>
2024-08-14 18:59:54 +0530

    Created urls and views

--- socialmedia/views.py
+++ socialmedia/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 # from django.views import View
 from .models import Profile, Post, Comment
-from django.shortcuts import get_object_or_404
 
 
 class Home(TemplateView):
@@ -30,8 +29,8 @@ class UserProfile(TemplateView):
         return render(request, self.template_name, context)
 
 
-class Blog(TemplateView):
-    template_name = "user/blog.html"
+class BlogList(TemplateView):
+    template_name = "user/blog_list.html"
 
     def get(self, request):
         if not request.user.is_authenticated:
@@ -44,27 +43,27 @@ class Blog(TemplateView):
         return render(request, self.template_name, context=context)
 
 
+class ViewBlog(TemplateView):
+    template_name = "user/Viewblog.html"
+
+    def get(self, request, pk):
+        post = Post.objects.filter(id=pk).first()
+        comments = Comment.objects.filter(post=post)
+        context = {
+            "post": post,
+            "comments": comments
+        }
+        return render(request, self.template_name, context=context)
+
+
 class AddCommentView(TemplateView):
     template_name = "user/blog.html"
 
-    def post(self, request, pk):
-        post = Post.objects.get(id=pk)
-        comment_text = request.POST.get('comment')
-        if comment_text:
-            Comment.objects.create(post=post, user=request.user, comment=comment_text)
-        return redirect('/')
-
-
-# class AddCommentView(View):
-#     def post(self, request, pk):
-#         try:
-#             post = Post.objects.get(id=uuid.UUID(pk))
-#         except Post.DoesNotExist:
-#             return redirect('blog_list')  # Redirect if post does not exist
-
-#         comment_text = request.POST.get('comment')
-#         if comment_text:
-#             Comment.objects.create(post=post, user=request.user, comment=comment_text)
-
-#         return redirect('blog_list')
-
+    def get(self, request, pk):
+        post = Post.objects.filter(id=pk).first()
+        comments = Comment.objects.filter(post=post)
+        print(comments)
+        context = {
+            "comments": comments
+        }
+        return render(request, self.template_name, context=context)
