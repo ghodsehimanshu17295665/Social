4bcceae6fc65bd61465b8a42475982f86bf45343
Himanshu <himanshughodse@gmail.com>
2024-08-22 11:33:18 +0530

    Registered models in admin file

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -1,11 +1,61 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Post, Comment, Like, Follow
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ("id", "email")
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 @admin.register(Profile)
