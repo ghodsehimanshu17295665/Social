c741925410d4a88ec9927ed2e92619268583de71
Himanshu <himanshughodse@gmail.com>
2024-08-22 19:06:44 +0530

    Created forms and Updated

--- socialmedia/forms.py
+++ socialmedia/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from socialmedia.models import Post
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 # from django.contrib.auth.models import User
-from .models import User
+from .models import User, Profile, Post
 
 
 class PostForm(forms.ModelForm):
@@ -13,10 +13,12 @@ class PostForm(forms.ModelForm):
 
 # class SignUpForm(UserCreationForm):
 #     email = forms.EmailField(required=True)
+#     first_name = forms.CharField(required=True)
+#     last_name = forms.CharField(required=True)
 
 #     class Meta:
 #         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
+#         fields = ['username', 'email', 'firstname', 'lastname', 'password1', 'password2']
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -37,3 +39,13 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("email",)
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'birth_date', 'bio', 'location', 'gender']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'bio': forms.Textarea(attrs={'rows': 4}),
+        }
