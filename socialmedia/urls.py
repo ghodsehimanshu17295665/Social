3065bdabca23ebd659cc61078e00eb6bac6171bc
Himanshu <himanshughodse@gmail.com>
2024-08-22 11:35:05 +0530

    Created Urls

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import Home, LoginSignup, UserProfile, BlogList, ViewBlog, AddCommentView
+from .views import Home, Login, UserProfile, BlogList, ViewBlog, AddCommentView
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home_page'),
-    path('login/', LoginSignup.as_view(), name='login_signup'),
-    path('profile/', UserProfile.as_view(), name='profile'),
+    path('login/', Login.as_view(), name='login'),
+    # path('signup/', Signup.as_view(), name='signup'),
     path('blog/list/', BlogList.as_view(), name='blog_list'),
     path('blog/<uuid:pk>/', ViewBlog.as_view(), name='blogview'),
     path('blog/<uuid:pk>/add_comment/', AddCommentView.as_view(), name='add_comment'),
+    path('profile/<uuid:pk>/', UserProfile.as_view(), name='profile'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
