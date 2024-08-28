45592e59cf3051081b2ce1aa53f878d310861b4d
Himanshu <himanshughodse@gmail.com>
2024-08-28 15:54:16 +0530

    created urls

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -16,6 +16,7 @@ from .views import (
     Myblog,
     Updateblog,
     FollowUserView,
+    ActivateAccountView,
 )
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,31 +25,33 @@ from django.conf import settings
 urlpatterns = [
     # Home Page
     path("", Home.as_view(), name="home_page"),
-
     # signUp/Login/Logout Page
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-
     # User Profile related page
     path("profile/<uuid:pk>/", UserProfile.as_view(), name="profile"),
     path("profile/update/", UpdateProfile.as_view(), name="update_profile"),
     path("create/post/", CreatePost.as_view(), name="create_post"),
     path("post/<uuid:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("blog/update/<uuid:pk>/", Updateblog.as_view(), name="update_blog"),
-
     # list all user related
     path("my/blog/", Myblog.as_view(), name="my_blog"),
     path("blog/list/", BlogList.as_view(), name="blog_list"),
     path("blog/<uuid:pk>/", ViewBlog.as_view(), name="blogview"),
-    path('profile/page/', UserProfileView.as_view(), name='profilePage'),
+    path("profile/page/", UserProfileView.as_view(), name="profilePage"),
     path(
         "blog/<uuid:pk>/add_comment/",
         AddCommentView.as_view(),
         name="add_comment",
     ),
     path("post/<uuid:pk>/like/", LikePostView.as_view(), name="like_post"),
-
     # Follow
-    path('follow/<uuid:pk>/', FollowUserView.as_view(), name='follow_user'),
+    path("follow/<uuid:pk>/", FollowUserView.as_view(), name="follow_user"),
+    # email verification
+    path(
+        "activate/<str:uid>/<str:token>/",
+        ActivateAccountView.as_view(),
+        name="activate",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
