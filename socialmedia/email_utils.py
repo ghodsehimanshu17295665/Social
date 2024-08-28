9db10f564c30c7f93e2597457ad88c000400f636
Himanshu <himanshughodse@gmail.com>
2024-08-28 15:51:40 +0530

    Created function to send emails

--- socialmedia/email_utils.py
+++ socialmedia/email_utils.py
@@ -1,11 +1,22 @@
-# email_utils.py
-from django.core.mail import send_mail
-from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
 
 
-def send_simple_email(user):
-    subject = 'Welcome to Django Server'
-    message = f'Hi {user.username}, thank you for registering on Django Server.'
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [user.email, ]
-    send_mail(subject, message, email_from, recipient_list)
+def send_verification_email(user, request):
+    user.generate_verification_token()
+    current_site = get_current_site(request)
+    mail_subject = 'Activate your account'
+    message = render_to_string('registration/acc_active_email.html', {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': str(user.id),
+        'token': user.verification_token,
+    })
+    email = EmailMultiAlternatives(
+        subject=mail_subject,
+        body=message,
+        to=[user.email],
+    )
+    email.attach_alternative(message, "text/html")
+    email.send()
