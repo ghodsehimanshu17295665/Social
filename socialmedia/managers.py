b1fbb8412f249b2485b8ba50a1433df11b386bf5
Himanshu <himanshughodse@gmail.com>
2024-09-04 17:51:24 +0530

    define custom managers

--- socialmedia/managers.py
+++ socialmedia/managers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError("The Email field must be set")
 
         # Corrected line: Normalize the email
         email = self.normalize_email(email)
@@ -14,8 +14,8 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
         return self.create_user(email, password, **extra_fields)
