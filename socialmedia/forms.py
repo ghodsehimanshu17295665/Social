6573e60035db48e231bd5107b702bd83393d0215
Himanshu <himanshughodse@gmail.com>
2024-10-24 15:22:13 +0530

    Create forms

--- socialmedia/forms.py
+++ socialmedia/forms.py
@@ -23,7 +23,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["email", "password1", "password2"]
 
 
 class CustomUserCreationForm(UserCreationForm):
